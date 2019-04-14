880b0a115f686037dff14eecf7697c6a91c9f05f
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-14 18:39:18 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ clickbait.posts.create!(title: "10 Ways You Are Already Awesome")
 clickbait.posts.create!(title: "This Yoga Strech Cures Procrastination, Maybe")
 clickbait.posts.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
 
-movies = category.create!(name: "Movies")
+movies = Category.create!(name: "Movies")
 movies.posts.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog")
